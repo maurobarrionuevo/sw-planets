8ab5023cf2a23cedb6bf649cd4ed024482fcd315
Mauro Barrionuevo <mauro.barrionuevo@live.com>
2021-12-02 16:05:57 -0300

    Added propTypes and defaultProps example

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const ListItem = ({ planet, idx, onPlanetClick }) => {
     return <li onClick={() => onPlanetClick(idx + 1)} key={planet.url}>{planet.name}</li>
 }
 
 export default ListItem
+
+
+ListItem.defaultProps = {
+    planet: 'Some planet',
+}
+
+ListItem.propTypes = {
+    planet: PropTypes.string,
+    idx: PropTypes.number,
+    onPlanetClick: PropTypes.func
+}
